dfa70da42cfdaa2072199663b37060df1418c95b
Alissa Balge <alissa.balge@gmail.com>
2018-11-10 16:30:50 +0000

    edited read me and added screeshot

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
+
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :email, :presence => true, :uniqueness => true
+
 end
